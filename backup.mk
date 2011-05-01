7582904eba7ea588370adc902b396cfa17fda8a9
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-05-01 15:41:07 -0500

    Changes To Directory
    Change-Id: I8eca699a867c39a566c92b3105e95f68e456fb69

--- backup.mk
+++ backup.mk
@@ -41,12 +41,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxVdec \
     lights.thunderg \
-    gralloc.thunderg \
-    copybit.thunderg \
-    gps.thunderg
+    gps.thunderg \
+    copybit.thunderg
 
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.sf.lcd_density=160 \
    ro.com.google.clientidbase=android-hms-tmobile-us \
    ro.com.google.clientidbase.gmm=android-lge \
    ro.com.google.clientidbase.yt=android-lge \
@@ -56,45 +54,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
    ro.com.google.networklocation=1 \
    ro.com.google.gmsversion=2.2_r5 \
    ro.setupwizard.enable_bypass=1 \
-   ro.ril.disable.power.collapse=1 \
+   ro.ril.disable.power.collapse = 1 \
    rild.libpath=/system/lib/libril-qc-1.so
    rilswitch.vendorlibpath=/system/lib/libril-qc-1.so \
    rilswitch.ganlibpath=/system/lib/libganril.so \
-   ro.ril.gprsclass=12 \
+   ro.ril.gprsclass = 12 \
    ro.ril.hsxpa=2 \
-   wifi.supplicant_scan_interval=45 \
+   wifi.supplicant_scan_interval = 15 \
+   ro.sf.lcd_density = 160 \
    ro.opengles.version=131072 \
    ro.ril.enable.dtm=0 \
    ro.ril.hsdpa.category=8 \
    ro.ril.hsupa.category=5 \
-   debug.sf.hw=1 \
    mobiledata.interfaces=gannet0,rmnet0,rmnet1,rmnet2
-
-# Default network type
-# 0 => WCDMA Preferred.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=0 \
-
-# Performences tweaks
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.heapsize=32m \
-    ro.compcache.default=0 \
-    persist.sys.use_dithering=1 \
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
-# Keylayouts and Keychars
+# Keylayouts
 PRODUCT_COPY_FILES += \
-    device/lge/thunderg/keylayout/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
-    device/lge/thunderg/keylayout/keylayout/AVCRP.kl:system/usr/keylayout/AVCRP.kl \
-    device/lge/thunderg/keylayout/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/lge/thunderg/keylayout/keylayout/thunder_keypad.kl:system/usr/keylayout/thunder_keypad.kl \
-    device/lge/thunderg/keychars/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    device/lge/thunderg/keychars/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
-    device/lge/thunderg/keychars/keychars/thunder_keypad.kcm.bin:system/usr/keychars/thunder_keypad.kcm.bin
+    vendor/lge/thunderg/proprietary/usr/keylayout/thunder_keypad.kl:system/usr/keylayout/thunder_keypad.kl \
+    vendor/lge/thunderg/proprietary/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    vendor/lge/thunderg/proprietary/usr/keychars/thunder_keypad.kcm.bin:system/usr/keychars/thunder_keypad.kcm.bin \
    
 
 # Board-specific init (does not support charging in "power off" state yet)
@@ -122,10 +104,18 @@ PRODUCT_COPY_FILES += \
     device/lge/thunderg/ramdisk/chargerimages/black_bg.rle:root/chargerimages/black_bg.rle \
     device/lge/thunderg/ramdisk/chargerimages/battery_bg.rle:root/chargerimages/battery_bg.rle
 
+# 2D (using proprietary because of poor perfomance of open source libs)
+PRODUCT_COPY_FILES += \
+    vendor/lge/thunderg/proprietary/lib/hw/gralloc.default.so:system/lib/hw/gralloc.default.so \
+    vendor/lge/thunderg/proprietary/lib/hw/gralloc.thunderg.so:system/lib/hw/gralloc.thunderg.so \
 
-# Media Configuration XML File
+# Media Configuration Xml File
 PRODUCT_COPY_FILES += \
     device/lge/thunderg/media_profiles.xml:system/etc/media_profiles.xml
+
+# Auto Volume Fail
+PRODUCT_COPY_FILES += \
+    device/lge/thunderg/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt 
 
 # Sensors
 PRODUCT_COPY_FILES += \
@@ -157,16 +147,18 @@ PRODUCT_COPY_FILES += \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc.bin:system/etc/wl/rtecdc.bin \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc-apsta.bin:system/etc/wl/rtecdc-apsta.bin \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc-mfgtest.bin:system/etc/wl/rtecdc-mfgtest.bin \
+    vendor/lge/thunderg/proprietary/bin/wl:system/bin/wl \
+    vendor/lge/thunderg/proprietary/bin/wpa_supplicant:system/bin/wpa_supplicant 
 
 # SD Card
 PRODUCT_COPY_FILES += \
-    device/lge/thunderg/vold.fstab:system/etc/vold.fstab \
+    vendor/lge/thunderg/proprietary/etc/vold.fstab:system/etc/vold.fstab \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    vendor/lge/thunderg/proprietary/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/lge/thunderg/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
     vendor/lge/thunderg/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
-    device/lge/thunderg/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    vendor/lge/thunderg/proprietary/lib/liba2dp.so:system/lib/liba2dp.so \
 
 # GPS
 PRODUCT_COPY_FILES += \
@@ -185,20 +177,40 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml 
 
-# Neccesary Files
+# LGE services
 PRODUCT_COPY_FILES += \
     vendor/lge/thunderg/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/lge/thunderg/proprietary/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    vendor/lge/thunderg/proprietary/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    vendor/lge/thunderg/proprietary/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+    vendor/lge/thunderg/proprietary/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
+
+# rmt_storage (What is this?)
+PRODUCT_COPY_FILES += \
+    vendor/lge/thunderg/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+
+# wipeirface (What is this?)
+PRODUCT_COPY_FILES += \
+    vendor/lge/thunderg/proprietary/bin/wiperiface:system/bin/wiperiface \
 
 # Touchscreen firmware updater
 PRODUCT_COPY_FILES += \
     vendor/lge/thunderg/proprietary/bin/tsdown:system/bin/tsdown \
     vendor/lge/thunderg/proprietary/etc/MELFAS_FIRM.bin:system/etc/MELFAS_FIRM.bin \
 
+# netmgr (What is this?)
+PRODUCT_COPY_FILES += \
+    vendor/lge/thunderg/proprietary/bin/netmgrd:system/bin/netmgrd \
+    vendor/lge/thunderg/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+    vendor/lge/thunderg/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so
+
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lge.vibrator_amp=125 \
 
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
+# Move dalvik-cache to /data
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1 \
 
 # RIL
 PRODUCT_COPY_FILES += \
@@ -233,19 +245,49 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lge/thunderg/proprietary/lib/libril.so:system/lib/libril.so \
 
+# OMX
+PRODUCT_COPY_FILES += \
+    vendor/lge/thunderg/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAdpcmDec.so:system/lib/libOmxAdpcmDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAmrDec.so:system/lib/libOmxAmrDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
+    vendor/lge/thunderg/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     vendor/lge/thunderg/proprietary/bin/BCM4325D1_004.002.004.0218.0248.hcd:system/bin/BCM4325D1_004.002.004.0218.0248.hcd \
 
+# A "special" libcrypto for Kineto
+PRODUCT_COPY_FILES += \
+    device/lge/thunderg/prebuilt/libcrypto98.so:system/lib/libcrypto98.so \
+
 # Files Needed For Wifi Calling
 PRODUCT_COPY_FILES += \
+    device/lge/thunderg/prebuilt/app/Kineto.apk:system/app/Kineto.apk \
+    vendor/lge/thunderg/proprietary/lib/libkineto.so:/system/lib/libkineto.so \
     vendor/lge/thunderg/proprietary/lib/libganril.so:system/lib/libganril.so \
-    vendor/lge/thunderg/proprietary/lib/librilswitch.so:system/lib/librilswitch.so
+    vendor/lge/thunderg/proprietary/lib/librilswitch.so:system/lib/librilswitch.so \
 
 PRODUCT_LOCALES += mdpi
 
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_thunderg
+PRODUCT_NAME := generic_thunderg
+PRODUCT_BRAND := LGE
 PRODUCT_DEVICE := thunderg
+PRODUCT_MODEL := LG-P509
+PRODUCT_MANUFACTURER := LGE
