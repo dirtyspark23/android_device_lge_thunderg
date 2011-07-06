2680b499d9266f8caa23dbb2d4aab4cc0d137fc1
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-07-06 04:02:13 -0500

    Attempt to fix camera... ugh
    Change-Id: I5d00903d3be6eef95da7dd30e4c2380820207bf3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,8 @@ BOARD_USE_KINETO_COMPATIBILITY := true
 
 BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/lge/thunderg/recovery_ui.c
 
+BUILD_LIBCAMERA := true
+
 BOARD_USES_QCOM_GPS := true
 BOARD_GPS_LIBRARIES := libloc_api
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := thunderg
@@ -49,8 +51,6 @@ WIFI_DRIVER_MODULE_NAME := "wireless"
 WIFI_DRIVER_FW_STA_PATH := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_FW_AP_PATH := "/system/etc/wl/rtecdc-apsta.bin"
 WIFI_DRIVER_HAS_LGE_SOFTAP := true
-
-BOARD_EGL_CFG := device/lge/thunderg/egl.cfg
 
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=thunderg
 BOARD_KERNEL_BASE := 0x12800000
