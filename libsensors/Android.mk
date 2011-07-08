eba40e63ac9b5c8c13b0fc525fef973fdffad512
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-07-07 22:22:05 -0500

    Start work on libsensors
    Change-Id: I41e82398348828ec50e4e1df7c2c5cb90640c8e5

---
+++ libsensors/Android.mk
@@ -0,0 +1,15 @@
+
+
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation, not prelinked and stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),thunderg)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SRC_FILES := sensors.cpp
+LOCAL_MODULE := sensors.thunderg
+include $(BUILD_SHARED_LIBRARY)
