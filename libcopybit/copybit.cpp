25b0ef85fb9979191a303f0fdbcc312b676d3c64
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-07-13 22:00:03 -0500

    Update CAF code
    Change-Id: I87f8971656dd923694b6a790eb94cde660ac83cf

--- libcopybit/copybit.cpp
+++ libcopybit/copybit.cpp
@@ -124,8 +124,10 @@ static int get_format(int format) {
     case COPYBIT_FORMAT_RGB_888:       return MDP_RGB_888;
     case COPYBIT_FORMAT_RGBA_8888:     return MDP_RGBA_8888;
     case COPYBIT_FORMAT_BGRA_8888:     return MDP_BGRA_8888;
+    //case COPYBIT_FORMAT_YCrCb_422_SP:  return MDP_Y_CBCR_H2V1;
     case COPYBIT_FORMAT_YCrCb_420_SP:  return MDP_Y_CBCR_H2V2;
     case COPYBIT_FORMAT_YCbCr_422_SP:  return MDP_Y_CRCB_H2V1;
+    //case COPYBIT_FORMAT_YCbCr_420_SP:  return MDP_Y_CRCB_H2V2;
     }
     return -1;
 }
@@ -197,7 +199,7 @@ static void set_rects(struct copybit_context_t *dev,
 static void set_infos(struct copybit_context_t *dev, struct mdp_blit_req *req) {
     req->alpha = dev->mAlpha;
     req->transp_mask = MDP_TRANSP_NOP;
-    req->flags = dev->mFlags | MDP_BLEND_FG_PREMULT;
+    req->flags = dev->mFlags;// | MDP_BLEND_FG_PREMULT;
 }
 
 /** copy the bits */
@@ -462,7 +464,7 @@ static int open_copybit(const struct hw_module_t* module, const char* name,
     } else {
         struct fb_fix_screeninfo finfo;
         if (ioctl(ctx->mFD, FBIOGET_FSCREENINFO, &finfo) == 0) {
-            if (strcmp(finfo.id, "msmfb") == 0) {
+            if (strncmp(finfo.id, "msmfb", 5) == 0) {
                 /* Success */
                 status = 0;
             } else {
